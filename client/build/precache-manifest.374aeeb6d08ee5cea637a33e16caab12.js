80352fd098dedffb384ab9dd3613ff8a67527b06
Adventure 2019 <caliew26@gmail.com>
2020-04-09 14:15:35 -0400

    updated risk security

---
+++ client/build/precache-manifest.374aeeb6d08ee5cea637a33e16caab12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6e681239b64e13e414cd",
+    "url": "/static/js/main.161bd9d9.chunk.js"
+  },
+  {
+    "revision": "34139c9efba343af1f00",
+    "url": "/static/js/2.fe167b31.chunk.js"
+  },
+  {
+    "revision": "6e681239b64e13e414cd",
+    "url": "/static/css/main.08f88839.chunk.css"
+  },
+  {
+    "revision": "f287c3b5ae04c6a0c57346d578491869",
+    "url": "/index.html"
+  }
+];
